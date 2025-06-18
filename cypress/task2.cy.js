ec581bc945204bba4270a596af735b00f61bad76
Daniel Norman <bapo@pop-os.local>
2025-06-18 15:41:07 +0300

    task 2

--- cypress/task2.cy.js
+++ cypress/task2.cy.js
@@ -1,77 +1,76 @@
 describe("template spec", () => {
   beforeEach(() => {
-    // Wait 10 seconds to allow manual captcha completion
+    // Set viewport to 1920x1080
+    cy.viewport(1920, 1080); //
+
+    // Handle uncaught exceptions on amazon.com
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      // Returning false here prevents Cypress from failing the test
+      return false;
+    });
+
+    // Wait 15 seconds to allow manual captcha completion
     cy.visit("https://amazon.com");
-    cy.wait(10000);
+    cy.wait(15000);
+    addToCartFromSearch(
+      "Bostitch Personal Electric Pencil Sharpener, Powerful Stall-Free Motor, High Capacity Shavings Tray, Blue (EPS4-BLUE)",
+      1
+    );
   });
 
-  it("Validate navigation elements on top menu and customer service page search", () => {
-    // Wait for the navigation container to be visible
-    cy.get("#nav-xshop-container").should("be.visible");
-
-    // Define the expected navigation items in order
-    const expectedNavItems = [
-      "Today's Deals",
-      "Registry",
-      "Prime Video",
-      "Gift Cards",
-      "Customer Service",
-      "Sell",
-    ];
-
-    // Get all navigation list items
-    cy.get("#nav-xshop-container .nav-li").should(
-      "have.length.at.least",
-      expectedNavItems.length
-    );
-
-    // Validate each navigation item exists, is in correct order, and is clickable
-    expectedNavItems.forEach((itemText, index) => {
-      cy.get("#nav-xshop-container .nav-li")
-        .eq(index)
-        .within(() => {
-          // Check that the element contains the expected text
-          cy.get(".nav-a").should("contain.text", itemText);
-
-          // Verify the link is clickable (has href attribute and is not disabled)
-          cy.get(".nav-a").should("have.attr", "href");
-          cy.get(".nav-a").should("not.have.attr", "disabled");
-          cy.get(".nav-a").should("be.visible");
-
-          // Verify the link structure is correct
-          cy.get(".nav-a").should("have.class", "nav-a");
-        });
-    });
-
-    // Additional validation: ensure all links are actually clickable
-    cy.get("#nav-xshop-container .nav-a").each(($link) => {
-      // Check that the link has a valid href
-      cy.wrap($link).should("have.attr", "href").and("not.be.empty");
-
-      // Check that the link is visible and not disabled
-      cy.wrap($link).should("be.visible");
-      cy.wrap($link).should("not.be.disabled");
-    });
-
-    // Find and click on the Customer Service link
-    cy.get("#nav-xshop-container .nav-a")
-      .contains("Customer Service")
+  afterEach(() => {
+    //clear cart
+    cy.get("[data-feature-id='item-delete-button']")
+      .first()
       .should("be.visible")
       .click();
+    cy.get("[data-feature-id='item-delete-button']")
+      .first()
+      .should("be.visible")
+      .click();
+  });
 
-    // Verify we navigated to the customer service page
-    cy.url().should("include", "nav_cs_customerservice");
-
-    // Type "where is My Stuff" in the search box and press enter
+  // Function to add item to cart by searching for it
+  function addToCartFromSearch(searchTermm, productCount) {
+    // Wait for the search box to be visible
     cy.get("#twotabsearchtextbox")
       .should("be.visible")
       .clear()
-      .type("where is My Stuff{enter}");
+      .type(searchTermm + "{enter}");
 
-    // Click on "Your Amazon orders" element
-    cy.contains("Your Amazon orders").should("be.visible").click();
+    // Wait for search results to load
+    cy.get("[data-component-type='s-search-results']").should("be.visible");
 
-    // Validate that we are redirected to login page
-    cy.url().should("include", "signin");
+    // Click add to cart on the first product found
+    cy.get("#a-autoid-1-announce").should("be.visible").click();
+
+    // Verify item was added to cart
+    cy.get(
+      ".a-section.a-spacing-none.ewc-item-content.ewc-item-update-content"
+    ).should("have.length", productCount);
+  }
+
+  it("Validate cart", () => {
+    // Wait for the navigation container to be visible
+    cy.get("#nav-xshop-container").should("be.visible");
+
+    cy.visit(
+      "https://www.amazon.com/Scissors-iBayam-Crafting-Scrapbooking-Knitting/dp/B07H3QKN2Z"
+    );
+    cy.get("[alt='Yellow, Grey, Blue']").eq(1).click({ force: true }); //may be a better selector, the force is because is covered by another element
+    cy.get("#inline-twister-expanded-dimension-text-color_name", {
+      timeout: 30000,
+    }) // timeout can be configured globaly, noticed that sometimes it takes longger to appear
+      .should("be.visible")
+      .should("have.text", "Yellow, Grey, Blue");
+
+    cy.get("#add-to-cart-button").should("be.visible").click(); // click on add to cart button
+    // Verify item was added to cart
+    cy.get(
+      ".a-section.a-spacing-none.ewc-item-content.ewc-item-update-content"
+    ).should("have.length", 2); // verify item was added to cart (2 items in the cart)
+    cy.get("#sw-gtc").should("be.visible").click(); // click on got to cart button
+    cy.get("[data-a-selector='increment']").eq(1).click(); // click on increment button on the second item on the cart
+    cy.get("[data-a-selector='increment']").eq(1).click();
   });
 });
